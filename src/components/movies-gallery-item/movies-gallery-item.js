c4f18071ae6be1fc92dc415de52f75604a03213f
Valentyn Solop <9solop@gmail.com>
2021-03-23 20:16:56 +0200

    moved the logic of mark like to App.js

--- src/components/movies-gallery-item/movies-gallery-item.js
+++ src/components/movies-gallery-item/movies-gallery-item.js
@@ -1,46 +1,27 @@
-import React, { Component } from "react";
-import './movies-gallery-item.css';
+import React from "react";
 
-export default class MoviesGalleryItem extends Component {
+import "./movies-gallery-item.css";
 
-    constructor (props) {
-        super (props);
-        this.state = {
-            like: false
-        };
-        this.onMovieLike = this.onMovieLike.bind(this);
+function MoviesGalleryItem ({ movieName, movieLike, onToggleLiked }) {
+
+    let classNames = "btn-star btn-sm";
+
+    if (movieLike) {
+        classNames += " like";
     }
 
-    onMovieLike () {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
-
-    render() {
-
-        const {movieName, movieId} = this.props;
-        const {like} = this.state;
-        let classNames = "btn-star btn-sm";
-
-        if (like) {
-            classNames += " like";
-        }
-
-        return (
-            <div id={movieId} className="movies-gallery__item">
-                <div className="star-btn">
-                    <button 
-                        onClick={this.onMovieLike}
-                        className={classNames}>
-                        <i className="fa fa-star"> </i>
-                    </button>
-                </div>
-                <div className="img"> </div>
-                <div className="film-name"> {movieName} </div>
-                <div className="film-year"> year </div>
+    return (
+        <>
+            <div className="star-btn">
+                <button onClick={onToggleLiked} className={classNames}>
+                    <i className="fa fa-star"> </i>
+                </button>
             </div>
-        );
-    };
-};
+            <div className="img"> </div>
+            <div className="film-name"> {movieName} </div>
+            <div className="film-year"> year </div>
+        </>
+    );
+}
 
+export default MoviesGalleryItem;
--- src/components/movies-gallery/movies-gallery.js
+++ src/components/movies-gallery/movies-gallery.js
@@ -5,53 +5,29 @@ import MoviesGalleryItem from "../movies-gallery-item/movies-gallery-item";
 import "../app/app.css";
 import "./movies-gallery.css";
 
-function MoviesGallery ({movies, onAddMovie}) {
-    
+function MoviesGallery({ movies, onToggleLiked }) {
+    const elements = movies.map((item) => {
+        const { id } = item;
+        const { like } = item;
+        const { name } = item;
+        return (
+            <div key={id} className="movies-gallery__item">
+                <MoviesGalleryItem
+                    movieName={name}
+                    movieId={id}
+                    movieLike={like}
+                    onToggleLiked={() => onToggleLiked(id)}
+                />
+            </div>
+        );
+    });
+
     return (
         <div className="movies-gallery">
             <div className="movies-gallery__title"> Movies Gallery </div>
             <div className="movies-gallery__items">
                 <div className="row">
-                    <MoviesGalleryItem
-                        movieName={movies[0].name}
-                        movieId={movies[0].id}
-                        movieLike={movies[0].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[1].name}
-                        movieId={movies[1].id}
-                        movieLike={movies[1].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[2].name}
-                        movieId={movies[2].id}
-                        movieLike={movies[2].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[3].name}
-                        movieId={movies[3].id}
-                        movieLike={movies[3].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[4].name}
-                        movieId={movies[4].id}
-                        movieLike={movies[4].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[5].name}
-                        movieId={movies[5].id}
-                        movieLike={movies[5].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[6].name}
-                        movieId={movies[6].id}
-                        movieLike={movies[6].like}
-                    />
-                    <MoviesGalleryItem
-                        movieName={movies[7].name}
-                        movieId={movies[7].id}
-                        movieLike={movies[7].like}
-                    />
+                    {elements}
                 </div>
             </div>
         </div>
